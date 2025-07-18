03d571016a57e38e99a140e39b1aeef46428e52e
Mark Mc Naught <mark@thetechbattalion.com>
2025-07-18 12:01:49 +0100

    Init multi-DS agent

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bedrock_agentic_ds_team_id" {
-  description = "The ID of the Bedrock Agentic Data Science team."
-  value       = module.bedrock_agentic_ds_team.team_id
-}
-
-output "s3_bucket_name" {
-  description = "S3 bucket for data science artifacts."
-  value       = module.bedrock_agentic_ds_team.s3_bucket_name
-}
